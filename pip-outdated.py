adfe234d5ec123389741c34f05f93b4a726e1da6
Stacy Brock <kalrnux@gmail.com>
2023-07-25 20:59:15 -0700

    Add pip-outdated rule

---
+++ pip-outdated.py
@@ -0,0 +1,9 @@
+from thefuck.utils import for_app
+
+@for_app('pip')
+def match(command):
+    return ('pip list' in command.script
+            and 'A new release of pip is available' in command.output)
+
+def get_new_command(command):
+    return 'pip install --upgrade pip'
