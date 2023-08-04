d33dee25af097b3fd4d578e52933482c3158f79f
Stacy Brock <kalrnux@gmail.com>
2023-08-04 10:43:40 -0700

    Update pip-outdated search string due to typo in pip
    "A new release is available" has turned into
    "A new release available" LOLWUT

--- pip-outdated.py
+++ pip-outdated.py
@@ -3,7 +3,7 @@ from thefuck.utils import for_app
 @for_app('pip')
 def match(command):
     return ('pip list' in command.script
-            and 'A new release of pip is available' in command.output)
+            and 'A new release of pip' in command.output)
 
 def get_new_command(command):
     return 'pip install --upgrade pip'
