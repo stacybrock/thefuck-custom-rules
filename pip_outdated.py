8976a12cf3d4d6f575a39c6d361e0448ac1ed9da
Stacy Brock <brock@oregonstate.edu>
2023-08-07 17:28:29 -0700

    Add 'install' command to pip_outdated match conditions

--- pip_outdated.py
+++ pip_outdated.py
@@ -2,7 +2,7 @@ from thefuck.utils import for_app
 
 @for_app('pip')
 def match(command):
-    return ('pip list' in command.script
+    return (('pip list' in command.script or 'pip install' in command.script)
             and 'A new release of pip' in command.output)
 
 def get_new_command(command):
